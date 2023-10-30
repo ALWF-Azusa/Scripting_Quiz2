a86c1db54fbe7934c466a16edea70030aae13d7b
Shih Hsuan Yu <kevin0921799052@gmail.com>
2023-10-30 20:34:34 +0800

    update code
    nothing

---
+++ 3B017015_01.py
@@ -0,0 +1,42 @@
+n = eval(input("請出入金字塔高度："))
+for i in range(1, n+1, 2):
+    """
+    range (1 , n+1 , 2 ) 
+    if n = 5 then the number will be (1,6,2) 
+    it means  
+    3 (1+2)
+    5(3+2)
+    7(5+2)
+    so the number 7 is greater than 6 so the 7 won't be export
+    So answer is 3,5
+    """
+    print(" " * ((n - i) // 2) + "*" * i)
+    # input 9 it means Less than 10 so the i will be 1,3,5,7,9
+    """
+    stage 1 print("." * ((9 - 1) // 2) + "*" * i) it will print ....*
+    stage 2 print("." * ((9 - 3) // 2) + "*" * i) it will print ...***
+    stage 3 print("." * ((9 - 5) // 2) + "*" * i) it will print ..*****
+    以此類推
+    """
+
+for i in range(n-2, 0, -2):
+    """
+    range (n-2 , 0 , -2 ) 
+    if n = 5 then the number will be (3,0,-2) 
+    it means  
+    1 (3-2)
+    -1(1-2) 
+    so the number -1 is greater Less than 0 so the -1 won't be export
+    So answer is 3,5
+    """
+
+    print(" " * ((n - i) // 2) + "*" * i)
+    """
+     i 7,5,3,1
+     n 9,9,9,9
+    stage 1 print("." * ((9 - 7) // 2) + "*" * i) it will print .*******
+    stage 2 print("." * ((9 - 5) // 2) + "*" * i) it will print ..*****
+    stage 3 print("." * ((9 - 3) // 2) + "*" * i) it will print ...***
+    以此類推
+    """
+    
---
+++ 3B017015_02.py
@@ -0,0 +1,30 @@
+# 定義一個包含學生資料的字典
+student = {}
+
+# 讀取用戶輸入
+student["sid"] = input("請輸入您的學號：")
+student["sname"] = input("請輸入您的姓名：")
+student["fchina"] = float(input("請輸入您的國文成績："))
+student["fmath"] = float(input("請輸入您的數學成績："))
+student["finfo"] = float(input("請輸入您的電腦成績："))
+
+# 計算總分和平均分
+total_score = student["fchina"] + student["fmath"] + student["finfo"]
+  # 四捨五入
+total_score_round = round(total_score)
+average_score = round(total_score / 3, 2)
+
+# 判斷成績是否及格
+if average_score < 60:
+    result = "不及格"
+else:
+    result = "合格"
+
+# 印出結果
+print("-" * 30)
+print(f'{student["sname"]}({student["sid"]})同學您好：')
+print("以下是您的各科成績與分數評定\n")
+print(f'國文：{student["fchina"]} / 數學：{student["fmath"]} / 電腦：{student["finfo"]}')
+print(f'總分：{total_score_round} / 平均：{average_score}')
+print("-" * 30)
+print(f'成績判定：{result}')
